3732de93f7346d78908662513e7b53d385bda3a6
Tomer Sardjoe <tomer.sardjoe@wur.nl>
2025-08-14 14:49:28 +0200

    Fix reindexing issues

--- Modified scripts/rf_module.py
+++ Modified scripts/rf_module.py
@@ -57,6 +57,12 @@ def init_tables(table):
                'Ptr', 'R1tr', 'R0tr', 'Rtr', 'F1tr', 'F0tr', 'Ftr']
     performance = pd.DataFrame(0.0, index = np.arange(n_g), columns = metrics)
     performance.index = table.index.values
+
+    # Normalize indices/columns to consistent flattened labels
+    imp.index = flatten_index_like(imp.index)
+    imp.columns = flatten_index_like(imp.columns)
+    performance.index = flatten_index_like(performance.index)
+
     return imp, performance
 
 def preprocess_df(table, null_h, min_missing, min_present):
@@ -86,56 +92,56 @@ def update_performance(table, i, y_sets):
     """Update the performance table."""
     
     # Number of genomes present
-    table.loc[i, 'count'] = sum(list(y_sets[0]))
+    table.iloc[i, table.columns.get_loc('count')] = sum(list(y_sets[0]))
 
     cm_train = confusion_matrix(y_sets[1], y_sets[3])
     cm_test = confusion_matrix(y_sets[2], y_sets[4])
 
     # Training metrics
-    table.loc[i, 'TPtr'] = cm_train[1,1]
-    table.loc[i, 'FPtr'] = cm_train[0,1]
-    table.loc[i, 'TNtr'] = cm_train[0,0]
-    table.loc[i, 'FNtr'] = cm_train[1,0]
+    table.iloc[i, table.columns.get_loc('TPtr')] = cm_train[1,1]
+    table.iloc[i, table.columns.get_loc('FPtr')] = cm_train[0,1]
+    table.iloc[i, table.columns.get_loc('TNtr')] = cm_train[0,0]
+    table.iloc[i, table.columns.get_loc('FNtr')] = cm_train[1,0]
 
     # Testing metrics
-    table.loc[i, 'TPte'] = cm_test[1,1]
-    table.loc[i, 'FPte'] = cm_test[0,1]
-    table.loc[i, 'TNte'] = cm_test[0,0]
-    table.loc[i, 'FNte'] = cm_test[1,0]
+    table.iloc[i, table.columns.get_loc('TPte')] = cm_test[1,1]
+    table.iloc[i, table.columns.get_loc('FPte')] = cm_test[0,1]
+    table.iloc[i, table.columns.get_loc('TNte')] = cm_test[0,0]
+    table.iloc[i, table.columns.get_loc('FNte')] = cm_test[1,0]
 
     # Reports for recall, precision, f1, accuracy
     train_report = classification_report(y_sets[1], y_sets[3], output_dict=True, zero_division=0)
     test_report = classification_report(y_sets[2], y_sets[4], output_dict=True, zero_division=0)
 
     # Accuracy and error
-    table.loc[i, 'Etr'] = 1 - train_report['accuracy']
-    table.loc[i, 'Ete'] = 1 - test_report['accuracy']
-    table.loc[i, 'Atr'] = train_report['accuracy']
-    table.loc[i, 'Ate'] = test_report['accuracy']
+    table.iloc[i, table.columns.get_loc('Etr')] = 1 - train_report['accuracy']
+    table.iloc[i, table.columns.get_loc('Ete')] = 1 - test_report['accuracy']
+    table.iloc[i, table.columns.get_loc('Atr')] = train_report['accuracy']
+    table.iloc[i, table.columns.get_loc('Ate')] = test_report['accuracy']
 
     # Precision
-    table.loc[i, 'P1tr'] = train_report['1']['precision']
-    table.loc[i, 'P0tr'] = train_report['0']['precision']
-    table.loc[i, 'Ptr'] = train_report['macro avg']['precision']
-    table.loc[i, 'P1te'] = test_report['1']['precision']
-    table.loc[i, 'P0te'] = test_report['0']['precision']
-    table.loc[i, 'Pte'] = test_report['macro avg']['precision']
+    table.iloc[i, table.columns.get_loc('P1tr')] = train_report['1']['precision']
+    table.iloc[i, table.columns.get_loc('P0tr')] = train_report['0']['precision']
+    table.iloc[i, table.columns.get_loc('Ptr')] = train_report['macro avg']['precision']
+    table.iloc[i, table.columns.get_loc('P1te')] = test_report['1']['precision']
+    table.iloc[i, table.columns.get_loc('P0te')] = test_report['0']['precision']
+    table.iloc[i, table.columns.get_loc('Pte')] = test_report['macro avg']['precision']
 
     # Recall
-    table.loc[i, 'R1tr'] = train_report['1']['recall']
-    table.loc[i, 'R0tr'] = train_report['0']['recall']
-    table.loc[i, 'Rtr'] = train_report['macro avg']['recall']
-    table.loc[i, 'R1te'] = test_report['1']['recall']
-    table.loc[i, 'R0te'] = test_report['0']['recall']
-    table.loc[i, 'Rte'] = test_report['macro avg']['recall']
+    table.iloc[i, table.columns.get_loc('R1tr')] = train_report['1']['recall']
+    table.iloc[i, table.columns.get_loc('R0tr')] = train_report['0']['recall']
+    table.iloc[i, table.columns.get_loc('Rtr')] = train_report['macro avg']['recall']
+    table.iloc[i, table.columns.get_loc('R1te')] = test_report['1']['recall']
+    table.iloc[i, table.columns.get_loc('R0te')] = test_report['0']['recall']
+    table.iloc[i, table.columns.get_loc('Rte')] = test_report['macro avg']['recall']
 
     # F1
-    table.loc[i, 'F1tr'] = train_report['1']['f1-score']
-    table.loc[i, 'F0tr'] = train_report['0']['f1-score']
-    table.loc[i, 'Ftr'] = train_report['macro avg']['f1-score']
-    table.loc[i, 'F1te'] = test_report['1']['f1-score']
-    table.loc[i, 'F0te'] = test_report['0']['f1-score']
-    table.loc[i, 'Fte'] = test_report['macro avg']['f1-score']
+    table.iloc[i, table.columns.get_loc('F1tr')] = train_report['1']['f1-score']
+    table.iloc[i, table.columns.get_loc('F0tr')] = train_report['0']['f1-score']
+    table.iloc[i, table.columns.get_loc('Ftr')] = train_report['macro avg']['f1-score']
+    table.iloc[i, table.columns.get_loc('F1te')] = test_report['1']['f1-score']
+    table.iloc[i, table.columns.get_loc('F0te')] = test_report['0']['f1-score']
+    table.iloc[i, table.columns.get_loc('Fte')] = test_report['macro avg']['f1-score']
 
 def flatten_index_like(obj):
     def clean_entry(entry):
@@ -158,7 +164,8 @@ def fit_classifiers(table, results, params, output, checkpoint):
     params = [ntrees, depth, purity, nthreads]
     """
     n_g = table.shape[0]
-    table = table.transpose() #I think this is easier transposed
+    gene_labels = flatten_index_like(table.index.values)
+    table = table.transpose()
     start = 0
     if checkpoint != 0:
         results[0] = pd.read_csv(output + "/imp.csv", header=0, index_col=0)
@@ -166,6 +173,8 @@ def fit_classifiers(table, results, params, output, checkpoint):
         # Normalize immediately after loading
         results[0].index = flatten_index_like(results[0].index)
         results[0].columns = flatten_index_like(results[0].columns)
+        results[1].index = flatten_index_like(results[1].index)
+        results[1] = results[1].reindex(gene_labels)
 
         start = checkpoint
 
@@ -223,12 +232,14 @@ def fit_classifiers(table, results, params, output, checkpoint):
             print("\nWriting importance and performance matrices...\n")
             results[0].index = flatten_index_like(results[0].index)
             results[0].columns = flatten_index_like(results[0].columns)
+            results[1].index = gene_labels
             results[0].round(5).to_csv(output + "/imp.csv")
             results[1].round(5).to_csv(output + "/performance.csv")
 
     # Ensure final state is clean even if no checkpoint triggered
     results[0].index = flatten_index_like(results[0].index)
     results[0].columns = flatten_index_like(results[0].columns)
+    results[1].index = gene_labels
     results[0].round(5).to_csv(output + "/imp.csv")
     results[1].round(5).to_csv(output + "/performance.csv")
 
